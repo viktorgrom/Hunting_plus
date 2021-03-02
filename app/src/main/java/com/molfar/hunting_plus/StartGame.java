e14897ed421ac36f5c5fc3e2b1ee5added95c071
viktorgrom <0673503163v@gmail.com>
2021-03-02 10:46:21 +0200

    Initial commit

---
+++ app/src/main/java/com/molfar/hunting_plus/StartGame.java
@@ -0,0 +1,20 @@
+package com.molfar.hunting_plus;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartGame extends AppCompatActivity {
+
+    GameView gameView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+
+}
